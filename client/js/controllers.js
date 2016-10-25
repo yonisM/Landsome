f622e2bf29241f3c1f1b699073c09ee96ae01a2e
shuriye <khaalidmohamed8@gmail.com>
2016-10-25 23:15:02 +0300

    Add files via upload

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -76,11 +76,14 @@ function ($scope, $stateParams) {
 
 
 }])
-.controller('userCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('UserCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-
+    $scope.logoutAlert = "false";
+    $scope.triggerLogoutAlert = function (){
+    $scope.logoutAlert = "true";
+}
 
 }])
  
